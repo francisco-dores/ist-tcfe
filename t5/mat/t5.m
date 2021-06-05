feecdc9afd3c77f7a3fe62814b14081890a7c9d1
francisco-dores <franciscodores@tecnico.ulisboa.pt>
2021-06-05 20:27:54 +0100

    Falta o frequency response

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,16 +1,31 @@
-R1=;
-R2=;
-C1=;
-C2=;
+RI=;
+RF=;
+CI=;
+CF=;
+
+
+f_low = 1/(2*pi*CF*RF);
+f_high = 1/(2*pi*CI*RI);
+
+f_c=sqrt(f1*f2);
+
+
+%Input Impedance (f=f_c)
+
+ZI = RI + 1/(2*pi*f_c*CI);
+
+
+%Output Impedance (f=f_c)
+
+ZF = 1/(1/RF+1/1/(2*pi*f_c*CF));
+
+
+%Gain (f=f_c)
+
+A_v = -ZF/ZI
 
 
 
-
-
-
-
-
-
-
+%Frequency Response
 
 
