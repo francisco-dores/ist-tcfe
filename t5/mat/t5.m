a747d52994ef8bd600454400390d9be2a54074e2
francisco-dores <franciscodores@tecnico.ulisboa.pt>
2021-06-06 12:23:25 +0100

    theoretical mais ou menos encaminhado

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,40 +1,78 @@
-RI=1000;
-RF=1000;
-CI=220e-9;
-CF=220e-9;
+R1 = 500;
+R2 = 1e3;
+R3 = 300e3;
+R4 = 5e3;
+C1 = 220e-9;
+C2 = 110e-9;
 
 
-f_low = 1/(2*pi*CF*RF);
-f_high = 1/(2*pi*CI*RI);
+f_low = 1/(2*pi*C1*R1);
+f_high = 1/(2*pi*C2*R2);
 
-f_c=sqrt(f_low*f_high);
+%CENTRAL FREQUENCY
 
-
-%Input Impedance (f=f_c)
-
-ZI = RI + 1/(2*pi*f_c*CI);
+f_c=sqrt(f_low*f_high)
 
 
-%Output Impedance (f=f_c)
-
-ZF = (RF*1/(2*pi*CF))/(RF+1/(2*pi*CF));
-
-
-%Gain (f=f_c)
-
-Av = -ZF/ZI;
+%CENTRAL FREQUENCY DEVIATION
+cfreq_dev = abs(f_c-1000)
 
 
+%TRANSFER FUNCTION
 
-%Frequency Response
+%T = ((R1*C1*s)/(1+R1*C1*s))*(1+R3/R4)*(1/(1+R2*C2*s));
+
+
+%INPUT IMPEDANCE (f=f_c)
+
+%ZI = RI + 1/(2*pi*f_c*CI);
+
+
+%OUTPUT IMPEDANCE (f=1kHz)
+
+%ZF = (RF*1/(2*pi*CF))/(RF+1/(2*pi*CF));
+
+
+%GAIN (f=f_c)
+
+w=2*pi*f_c;
+s=j*w;
+T = ((R1*C1*s)/(1+R1*C1*s))*(1+R3/R4)*(1/(1+R2*C2*s));
+
+Av_c = abs(T)
+
+
+%GAIN (f=1kHz)
+
+w=2*pi*1000;
+s=j*w;
+T = ((R1*C1*s)/(1+R1*C1*s))*(1+R3/R4)*(1/(1+R2*C2*s));
+
+Av_1 = abs(T)
+
+
+%GAIN DEVIATION
+gain_dev = abs(Av_1-100)
+ 
+ 
+%GAIN DEVIATION dB
+Av_1_db = 20*log10(Av_1)
+gain_dev_db = abs(Av_1_db-40)
+
+
+%FREQUENCY RESPONSE
 
 f=logspace(1,8); %Hz
 for i=1:length(f)
 w=2*pi*f(i);
+s = j*w;
 
-ZI = RI + 1/(j*w*CI);
-ZF = (RF*1/(j*w*CF))/(RF+1/(j*w*CF)); %QUAL DAS DUAS FÓRMULAS??? ESTA OU...
-Av (i) = -ZF/ZI; 
+T(i) = ((R1*C1*s)/(1+R1*C1*s))*(1+R3/R4)*(1/(1+R2*C2*s));
+Av = abs(T);
+
+%ZI = RI + 1/(j*w*CI);
+%ZF = (RF*1/(j*w*CF))/(RF+1/(j*w*CF)); %QUAL DAS DUAS FÓRMULAS??? ESTA OU...
+%Av (i) = -ZF/ZI; 
 
 end
 
